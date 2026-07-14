7d57d624680f4bbd3df017633f65f5a49ac776bf
Robert Lupas <robertlupas09@gmail.com>
2026-07-14 15:42:18 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,59 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { ThemeToggle } from "@/components/theme-toggle.tsx"
+import { Card, CardHeader } from "@/components/ui/card.tsx"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { Github01Icon, Search01Icon } from "@hugeicons/core-free-icons"
+import { cn } from "@/lib/utils.ts"
+import { Input } from "@/components/ui/input.tsx"
+import { ButtonGroup } from "@/components/ui/button-group.tsx"
 
 export const Route = createFileRoute("/")({ component: App })
 
 function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
+    <div className="flex h-fit min-h-screen flex-col gap-4 p-2">
+      <div className="sticky px-8 py-2">
+        <Card size="sm">
+          <CardHeader>
+            <div className="flex items-center justify-between">
+              <h1 className="font-heading text-xl font-medium">DomainScan</h1>
+
+              <div className="flex items-center gap-2">
+                <a
+                  className={cn(
+                    buttonVariants({
+                      variant: "outline",
+                      size: "icon",
+                    })
+                  )}
+                  href={"https://github.com/robertlupas/domainscan"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  children={<HugeiconsIcon icon={Github01Icon} />}
+                />
+                <ThemeToggle />
+              </div>
+            </div>
+          </CardHeader>
+        </Card>
+      </div>
+
+      <div className="flex flex-1 items-center justify-center">
+        <Card className="w-full max-w-2xl">
+          <CardHeader>
+            <h2 className="font-heading text-lg font-medium">Search domain</h2>
+            <ButtonGroup className="w-full">
+              <Input
+                id="input-button-group"
+                placeholder="Enter a name or domain"
+              />
+              <Button variant="outline" size="icon">
+                <HugeiconsIcon icon={Search01Icon} />
+              </Button>
+            </ButtonGroup>
+          </CardHeader>
+        </Card>
       </div>
     </div>
   )

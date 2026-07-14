1789aaf9c9bd05e493605712227ea7cdc2360e63
Robert Lupas <robertlupas09@gmail.com>
2026-07-15 00:05:23 +0300

    Create domainList.ts

---
+++ src/lib/shared/domainList.ts
@@ -0,0 +1,38 @@
+import { getDomain } from "tldts"
+
+const prefixes = ["use", "join", "try", "my", "your"]
+const tlds = ["com", "app", "so"]
+
+export function getDomainList(nameOrDomain: string): string[] {
+  nameOrDomain = nameOrDomain.trim()
+  if (nameOrDomain.length === 0) return []
+
+  const domain = getDomain(nameOrDomain)
+
+  if (domain != null) return [domain]
+  else {
+    const words = nameOrDomain.split(/[^A-Za-z0-9-]+/).filter(Boolean)
+    const combined: string[] = []
+    const combinedWithTlds: string[] = []
+    combined.push(words.join(""))
+
+    for (const prefix of prefixes)
+      if (!words[0].startsWith(prefix))
+        combinedWithTlds.push(`${prefix}${words.join("")}`)
+
+    combined.push(words.join("-"))
+    for (const prefix of prefixes)
+      if (!words[0].startsWith(prefix))
+        combinedWithTlds.push(`${prefix}-${words.join("-")}`)
+
+    for (const prefix of prefixes)
+      if (!words[0].startsWith(prefix))
+        combinedWithTlds.push(`${prefix}-${words.join("")}`)
+
+    for (const tld of tlds)
+      for (const combinedWords of combined)
+        combinedWithTlds.push(`${combinedWords}.${tld}`)
+
+    return combinedWithTlds
+  }
+}

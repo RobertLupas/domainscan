09cd60b831515df18bbafd9a4190b0d0da021840
Robert Lupas <robertlupas09@gmail.com>
2026-07-19 21:29:10 +0300

    Search for different tlds when domain is searched

--- src/lib/shared/domainList.ts
+++ src/lib/shared/domainList.ts
@@ -1,4 +1,4 @@
-import { getDomain } from "tldts"
+import { parse } from "tldts"
 import { defaultPrefixList, defaultTldList } from "@/lib/shared/data.ts"
 
 export type Domain = {
@@ -17,33 +17,39 @@ export function getDomainList(
   nameOrDomain = nameOrDomain.trim().toLowerCase()
   if (nameOrDomain.length === 0) return []
 
-  const domain = getDomain(nameOrDomain)
-
-  if (domain != null) return [{ name: domain, price: undefined }]
-
-  const words = nameOrDomain.split(/[^A-Za-z0-9-]+/).filter(Boolean)
-  const combined: string[] = []
-
-  if (!separateWithHyphen) {
-    if (!addPrefixes) combined.push(words.join(""))
-    else
-      for (const prefix of prefixes)
-        if (!words[0].startsWith(prefix))
-          combined.push(`${prefix}${words.join("")}`)
-  } else {
-    if (!addPrefixes) combined.push(words.join("-"))
-    else
-      for (const prefix of prefixes)
-        if (!words[0].startsWith(prefix)) {
-          combined.push(`${prefix}-${words.join("-")}`)
-          // if (words.length > 1) combined.push(`${prefix}-${words.join("")}`)
-        }
-  }
-
   const combinedWithTlds: string[] = []
-  for (const tld of tlds)
-    for (const combinedWords of combined)
-      combinedWithTlds.push(`${combinedWords}.${tld}`)
+
+  const domain = parse(nameOrDomain)
+  if (domain.domain != null) {
+    combinedWithTlds.push(domain.domain)
+
+    for (const tld of tlds)
+      if (tld !== parse(domain.domain).publicSuffix)
+        combinedWithTlds.push(`${domain.domainWithoutSuffix}.${tld}`)
+  } else {
+    const combined: string[] = []
+    const words = nameOrDomain.split(/[^A-Za-z0-9-]+/).filter(Boolean)
+
+    if (!separateWithHyphen) {
+      if (!addPrefixes) combined.push(words.join(""))
+      else
+        for (const prefix of prefixes)
+          if (!words[0].startsWith(prefix))
+            combined.push(`${prefix}${words.join("")}`)
+    } else {
+      if (!addPrefixes) combined.push(words.join("-"))
+      else
+        for (const prefix of prefixes)
+          if (!words[0].startsWith(prefix)) {
+            combined.push(`${prefix}-${words.join("-")}`)
+            // if (words.length > 1) combined.push(`${prefix}-${words.join("")}`)
+          }
+    }
+
+    for (const tld of tlds)
+      for (const combinedWords of combined)
+        combinedWithTlds.push(`${combinedWords}.${tld}`)
+  }
 
   return combinedWithTlds.map((name) => ({
     name,
